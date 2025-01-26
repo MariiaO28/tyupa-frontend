16afc5a3fe9af9b5a519951c62336595a5f38c68
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-01-26 16:37:57 +0100

    init

--- src/Pages/PetPage/PetPage.jsx
+++ src/Pages/PetPage/PetPage.jsx
@@ -1,10 +1,11 @@
 import Logo from "../../Components/Logo/Logo";
 import PetInfo from "../../Components/PetInfo/PetInfo";
 import TyupaAnimationComponent from "../../Components/TyupaAnimationComponent/TyupaAnimationComponent";
+import styles from "./PetPage.module.css";
 
 export default function PetPage() {
   return (
-    <div className="container">
+    <div className={`container, ${styles.background}`}>
         <Logo/>
         <TyupaAnimationComponent/>
         <PetInfo/>
